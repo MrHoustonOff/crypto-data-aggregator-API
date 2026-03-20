5718198a59f807d1777c0101d3afb2e86a909b79
MrHouston <mrhoustonvr@yandex.ru>
2026-03-20 16:47:48 +0300

    feat(db): create users orm model

---
+++ app/modules/users/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from sqlalchemy import String, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
