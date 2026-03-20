4a3ca2af25fd6e166e6cd6a9e23dcd095bc9b1a8
MrHouston <mrhoustonvr@yandex.ru>
2026-03-21 01:03:46 +0300

    (core, alerts): add AlertService for alert management business logic

---
+++ app/modules/alerts/services.py
@@ -0,0 +1,32 @@
+from uuid import UUID
+from app.modules.alerts.repositories import AlertRepository
+from app.modules.alerts.schemas import AlertCreate
+from app.modules.users.models import Alert
+
+
+class AlertService:
+    def __init__(self, repository: AlertRepository):
+        self.repository = repository
+
+    async def create_alert(self, user_id: UUID, alert_in: AlertCreate) -> Alert:
+        webhook_str = str(alert_in.webhook_url)
+
+        return await self.repository.create_alert(
+            user_id=user_id,
+            ticker=alert_in.ticker,
+            target_price=alert_in.target_price,
+            condition=alert_in.condition,
+            webhook_url=webhook_str,
+        )
+
+    async def get_user_alerts(self, user_id: UUID) -> list[Alert]:
+        """
+        Получение всех алертов пользователя.
+        """
+        return await self.repository.get_user_alerts(user_id=user_id)
+
+    async def delete_alert(self, alert_id: UUID, user_id: UUID) -> bool:
+        """
+        Удаление алерта.
+        """
+        return await self.repository.delete_alert(alert_id=alert_id, user_id=user_id)
