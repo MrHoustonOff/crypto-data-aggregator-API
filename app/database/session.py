30da30f1f9ddb44218b9a113374a90e9e4c16b66
MrHouston <mrhoustonvr@yandex.ru>
2026-03-20 22:59:25 +0300

    fix(db): move echo tag from async_sessionmaker() to AsyncEngine for fixing sql logging

--- app/database/session.py
+++ app/database/session.py
@@ -1,12 +1,13 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncEngine
 from app.core.config import settings
 
-engine: AsyncEngine = create_async_engine(settings.database_url)
+engine: AsyncEngine = create_async_engine(
+    settings.database_url,
+    echo=True)
 
 async_session_factory = async_sessionmaker(
     bind=engine,
     expire_on_commit=False,
-    echo=True
 )
 
 async def get_db():
