0161f779ec04f046f23c55a8d63e93176e353464
MrHouston <mrhoustonvr@yandex.ru>
2026-03-22 19:58:50 +0300

    refactor(core, users): update register endpoint to return auto-generated API key

--- app/modules/users/router.py
+++ app/modules/users/router.py
@@ -4,7 +4,7 @@ from typing import Annotated
 
 from app.database.session import get_db
 from app.modules.users.models import User
-from app.modules.users.schemas import UserCreate, UserResponse
+from app.modules.users.schemas import UserCreate, UserResponse, UserRegisterResponse
 from app.modules.users.repositories import UserRepository
 from app.modules.users.services import UserService
 from app.modules.users.dependencies import CurrentUserDep
@@ -25,21 +25,29 @@ UserServiceDep = Annotated[UserService, Depends(get_user_service)]
 @users_router.post(
     "/register",
     summary="User registration",
-    response_model=UserResponse,
+    response_model=UserRegisterResponse,
     status_code=status.HTTP_201_CREATED,
     dependencies=[Depends(IPRateLimiter(requests=2, window=10))]
 )
 async def register_user(user_in: UserCreate, service: UserServiceDep):
     """Регистрация нового пользователя"""
 
-    new_user = await service.register(user_in=user_in)
-    if new_user is None:
+    result = await service.register(user_in=user_in)
+    if result is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User with this email already exists",
         )
 
-    return new_user
+    new_user, raw_api_key = result
+
+    return UserRegisterResponse(
+        id=new_user.id,
+        email=new_user.email,
+        is_active=new_user.is_active,
+        created_at=new_user.created_at,
+        raw_api_key=raw_api_key
+    )
 
 @users_router.get(
     "/me",
