f101f4f98796844f481f9aab78d8b6ce2cb78ee7
MrHouston <mrhoustonvr@yandex.ru>
2026-03-22 19:55:55 +0300

    refactor(core, users): remove api_key from input and add UserRegisterResponse

--- app/modules/users/schemas.py
+++ app/modules/users/schemas.py
@@ -7,7 +7,6 @@ class UserCreate(BaseModel):
     """Схема для регистрации нового пользователя (то, что принимаем от клиента)"""
 
     email: EmailStr
-    api_key: str = Field(min_length=8, max_length=150)
 
 
 class UserResponse(BaseModel):
@@ -19,3 +18,7 @@ class UserResponse(BaseModel):
     created_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
+
+class UserRegisterResponse(UserResponse):
+    """Схема ответа ТОЛЬКО при регистрации. Содержит сырой API ключ!"""
+    raw_api_key: str
