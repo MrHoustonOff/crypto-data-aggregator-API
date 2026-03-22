95933998fb71368e09a75fbfa7a59d45ec43b652
MrHouston <mrhoustonvr@yandex.ru>
2026-03-22 16:28:42 +0300

    feat(core, redis): create redit dependence for FastApi

---
+++ app/database/redis.py
@@ -0,0 +1,10 @@
+from redis.asyncio import Redis
+from app.core.config import settings
+
+redis_client = Redis.from_url(settings.REDIS_URL, decode_responses=True)
+
+async def get_redis() -> Redis:
+    """
+    Зависимость для FastAPI. 
+    """
+    yield redis_client
